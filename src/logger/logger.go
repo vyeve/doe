c1e68b2823ee616546c5218705cfe0d105c6b93b
vyeve <xobpaxa@ukr.net>
2021-01-24 09:49:52 +0200

    added logger

---
+++ src/logger/logger.go
@@ -0,0 +1,48 @@
+package logger
+
+import (
+	"os"
+
+	"github.com/sirupsen/logrus"
+)
+
+const (
+	// logTimeFormat represents time format in log messages
+	logTimeFormat  = "2006-01-02 15:04:05.99"
+	LogLevelEnvKey = "LOG_LEVEL"
+)
+
+type Logger interface {
+	Debug(args ...interface{})
+	Debugf(format string, args ...interface{})
+	Info(args ...interface{})
+	Infof(format string, args ...interface{})
+	Warn(args ...interface{})
+	Warnf(format string, args ...interface{})
+	Error(args ...interface{})
+	Errorf(format string, args ...interface{})
+	Fatal(args ...interface{})
+	Fatalf(format string, args ...interface{})
+	aws.Logger
+}
+
+type logger struct {
+	*logrus.Logger
+}
+
+func NewEnv() Logger {
+	customFormatter := new(logrus.TextFormatter)
+	customFormatter.TimestampFormat = logTimeFormat
+	customFormatter.FullTimestamp = true
+	l := logger{}
+	l.Logger = logrus.New()
+
+	l.SetFormatter(customFormatter)
+	mode := os.Getenv(LogLevelEnvKey)
+	if mode == "info" {
+		l.SetLevel(logrus.InfoLevel)
+	} else {
+		l.SetLevel(logrus.DebugLevel)
+	}
+	return l
+}
