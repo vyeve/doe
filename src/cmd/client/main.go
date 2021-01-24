356d4ed1bfe29fb79d53dc8fea414aee396743fd
vyeve <xobpaxa@ukr.net>
2021-01-24 12:33:15 +0200

    added deployment for docker

---
+++ src/cmd/client/main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"context"
+
+	server "doe/src/client"
+	"doe/src/data"
+	"doe/src/logger"
+
+	"go.uber.org/fx"
+)
+
+func main() {
+	ctx := context.Background()
+	var (
+		log logger.Logger
+		srv server.Server
+	)
+	app := fx.New(
+		logger.Module,
+		data.Module,
+		server.Module,
+		fx.Populate(&log),
+		fx.Populate(&srv),
+	)
+	defer app.Stop(ctx) // nolint: errcheck
+
+	if err := app.Start(ctx); err != nil {
+		panic(err)
+	}
+	if err := srv.Serve(); err != nil {
+		log.Error(err)
+	}
+}
---
+++ src/cmd/endpoint/main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"context"
+
+	"doe/src/endpoint"
+	"doe/src/logger"
+
+	"go.uber.org/fx"
+)
+
+func main() {
+	ctx := context.Background()
+	var (
+		log logger.Logger
+		srv endpoint.Router
+	)
+	app := fx.New(
+		logger.Module,
+		endpoint.Module,
+		fx.Populate(&log),
+		fx.Populate(&srv),
+	)
+	defer app.Stop(ctx) // nolint: errcheck
+	if err := app.Start(ctx); err != nil {
+		panic(err)
+	}
+	if err := srv.ListenAndServe(); err != nil {
+		log.Error(err)
+	}
+}
